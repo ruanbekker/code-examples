f8fe4c0d00512b575b21fa23ca61966f4a664cc3
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-11-11 12:01:03 +0200

    Create read_from_sqlite_groupby.py

---
+++ sqlite/python/read_from_sqlite_groupby.py
@@ -0,0 +1,12 @@
+import sqlite3
+conn = sqlite3.connect('database.db')
+conn.row_factory = sqlite3.Row
+cur = conn.cursor()
+cur.execute('select count(*) as num, city from people group by city')
+for row in rows:
+    print("{}, {}".format(row['num'], row['city']))
+
+# output:
+# 2, cape town
+# 1, johannesburg
+# 1, kroonstad
