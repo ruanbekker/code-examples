3286f1d5f6a1bc6d22a4e30467a9af53a1b32a3c
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-11-11 12:02:37 +0200

    Update read_from_sqlite_groupby.py

--- sqlite/python/read_from_sqlite_groupby.py
+++ sqlite/python/read_from_sqlite_groupby.py
@@ -3,6 +3,8 @@ conn = sqlite3.connect('database.db')
 conn.row_factory = sqlite3.Row
 cur = conn.cursor()
 cur.execute('select count(*) as num, city from people group by city')
+rows = cur.fetchall()
+
 for row in rows:
     print("{}, {}".format(row['num'], row['city']))
 
